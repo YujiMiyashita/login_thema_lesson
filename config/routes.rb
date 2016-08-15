0df8da96a53b75d350776ecbe54ef56674679c58
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-08-15 16:00:19 +0900

    完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
-  resources :users
+  resources :users, except: [:new, :destroy]
+  get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
